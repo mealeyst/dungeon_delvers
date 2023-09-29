661748bf1d2072bf8b7d8850b4f0461b2a0d505b
mealeyst <mealeyst@gmail.com>
2023-09-28 21:49:59 -0400

    Adding main menu

--- client/lib/main.ts
+++ client/lib/main.ts
@@ -1,9 +1,11 @@
+import { MainMenu } from './engine/gui/mainMenu'
 import { Stage } from './engine/stage/stage'
 
 document.addEventListener(
   'DOMContentLoaded',
   function () {
     new Stage()
+    new MainMenu()
   },
   false,
 )
