302f1a29fa45f5e47ca6657a15b03d123c099093
mealeyst <melaeyst@gmail.com>
2023-10-12 12:49:03 -0400

    Trying to add globbing imports for parcel

--- client/lib/engine/gui/index.ts
+++ client/lib/engine/gui/index.ts
@@ -1,4 +1,8 @@
 import * as GUI from '@babylonjs/gui'
+import male_portraits from '../../../public/images/portraits/male/*.png'
+import female_portraits from '../../../public/images/portraits/female/*.png'
+
+const portraits = { ...male_portraits, ...female_portraits }
 
 type Character = {
   name: string
@@ -7,20 +11,45 @@ type Character = {
   image: string
 }
 
-const characterButton = (character: Character) => {
-  const button = GUI.Button.CreateSimpleButton(
+const party = [
+  {
+    name: 'Elfman',
+    health_max: 10,
+    health_current: 10,
+    image: 'male_elf_jn_lg.png',
+  },
+]
+
+const characterButton = async (character: Character) => {
+  const button = GUI.Button.CreateImageButton(
     'character_button',
     character.name,
+    portraits[character.image],
   )
   button.width = '100px'
   button.height = '120px'
-  button.image
+  button.paddingLeft = '10px'
+  button.paddingRight = '10px'
+  if (button.image) {
+    button.image.width = '80px'
+  }
+  return button
 }
 
 export default () => {
   console.log('GUI')
   const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI('UI')
   const panel = new GUI.StackPanel()
+  panel.isVertical = false
   panel.paddingLeft = '10px'
   panel.paddingRight = '10px'
+  party.forEach(async character => {
+    const button = await characterButton(character)
+    panel.addControl(button)
+  })
+  panel.adaptHeightToChildren = true
+  panel.adaptWidthToChildren = true
+  panel.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT
+  panel.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM
+  advancedTexture.addControl(panel)
 }
